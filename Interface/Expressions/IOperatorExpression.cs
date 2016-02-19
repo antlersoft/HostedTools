bd12edc747cbfbe5dd80cc13f97e470e7f5940b8
Michael A. MacDonald <mars@antlersoft.com>
2016-02-18 21:46:24 -0500

    Add interfaces for different kinds of expressions

---
+++ Interface/Expressions/IOperatorExpression.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.antlersoft.HostedTools.Interface.Expressions
+{
+    public interface IOperatorExpression : IHtExpression
+    {
+        string OperatorName { get; }
+        IList<IHtExpression> Operands { get; } 
+    }
+}
