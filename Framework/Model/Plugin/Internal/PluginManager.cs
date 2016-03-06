1efb42daf87cf3500be1f0db26c03c218de8ef2c
Michael A. MacDonald <mars@antlersoft.com>
2016-03-06 17:41:13 -0500

    Add IPluginSource to framework

--- Framework/Model/Plugin/Internal/PluginManager.cs
+++ Framework/Model/Plugin/Internal/PluginManager.cs
@@ -1,30 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using com.antlersoft.HostedTools.Framework.Interface.Plugin;
 
 namespace com.antlersoft.HostedTools.Framework.Model.Plugin.Internal
 {
     [Export(typeof(IPluginManager))]
-    public class PluginManager : IPluginManager
+    [Export(typeof(IAfterComposition))]
+    public class PluginManager : HostedObjectBase, IPluginManager, IAfterComposition
     {
         [ImportMany]
         public IEnumerable<IPlugin> Imported;
+
+        [ImportMany] public IEnumerable<IPluginSource> ImportedSources;
+
+        private List<IPlugin> _allPlugins = new List<IPlugin>();  
  
         public IPlugin this[string key]
         {
             get
             {
-                return Imported.FirstOrDefault(p => p.Name == key);
+                return _allPlugins.FirstOrDefault(p => p.Name == key);
             }
         }
 
         public IEnumerable<IPlugin> Plugins
         {
-            get { return Imported; }
+            get { return _allPlugins; }
+        }
+
+        public void AfterComposition()
+        {
+            _allPlugins.AddRange(Imported);
+            foreach (var ps in ImportedSources)
+            {
+                _allPlugins.AddRange(ps.SourcePlugins);
+            }
         }
     }
 }
