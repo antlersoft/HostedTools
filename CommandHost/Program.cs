dc3a0915753de4c2202f246d0af7c0f7ed08d47d
mmacdonald <michael.macdonald@orbitlogic.com>
2018-09-26 11:27:11 -0400

    Make sure CommandHost runs in STAThread,
    Since it can end up invoking code that expects to be in GUI thread

--- CommandHost/Program.cs
+++ CommandHost/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace com.antlersoft.HostedTools.CommandHost
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             new CommandRunner().Run(args);
