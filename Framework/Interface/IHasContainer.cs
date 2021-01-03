62b4d167d3fa73a39d62b03bed280f3f73f96599
Michael MacDonald <googlecode@antlersoft.com>
2021-01-03 14:13:18 -0500

    Convert to all dotnet builds/dependencies; fix LogFacade name

---
+++ Framework/Interface/IHasContainer.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.Composition.Hosting;
+
+namespace com.antlersoft.HostedTools.Framework.Interface
+{
+    public interface IHasContainer : IHostedObject
+    {
+        CompositionContainer Container { get; }
+    }
+}
