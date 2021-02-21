7b6bef60f7e68831ff2396b038c113134f790302
Michael MacDonald <googlecode@antlersoft.com>
2021-02-21 14:51:35 -0500

    Revert "Non-working attempt to get WPF to work with dotnet core"
    This reverts commit cdcb05f49b4e3dec90c35f25c34acbaa0e359e12.

--- WpfHostTemplate/Properties/AssemblyInfo.cs
+++ WpfHostTemplate/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WpfHostTemplate")]
+[assembly: AssemblyTitle("$safeprojectname$")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfHostTemplate")]
+[assembly: AssemblyProduct("$safeprojectname$")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
