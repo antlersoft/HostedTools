adb1a9dc3e113a26d4535bd2bf2d5a1014e30d21
mmacdonald86 <michael.macdonald@groundtruth.com>
2021-06-21 17:36:46 -0400

    Add TableAlias class in Pipeline
    for generating unique alias name for tables in generated SQL statements

---
+++ Pipeline/TableAlias.cs
@@ -0,0 +1,16 @@
+﻿
+namespace com.antlersoft.HostedTools.Pipeline
+{
+    public class TableAlias
+    {
+        private int index;
+        private string prefix;
+
+        public TableAlias(string p=null)
+        {
+            prefix = p ?? "A";
+        }
+        public string Current => $"{prefix}{index}";
+        public string Next => $"{prefix}{++index}";
+    }
+}
