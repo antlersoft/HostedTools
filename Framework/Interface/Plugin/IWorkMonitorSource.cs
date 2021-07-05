d45d95f7fc1f703fb6baf71acf33efe4b59ecff4
mmacdonald86 <michael.macdonald@groundtruth.com>
2021-07-05 18:47:46 -0400

    Add a generic interface for obtaining an IWorkMonitor

---
+++ Framework/Interface/Plugin/IWorkMonitorSource.cs
@@ -0,0 +1,19 @@
+﻿
+namespace com.antlersoft.HostedTools.Framework.Interface.Plugin
+{
+    public interface IWorkMonitorSource : IHostedObject
+    {
+        /// <summary>
+        /// Produce a (potentially short-lived) IWorkMonitor instance.
+        /// 
+        /// Not guaranteed the return the same monitor each time.  Callers should not expect to hold the monitor
+        /// for a long time
+        /// <para>
+        /// The contract of this interface demands that it always return a valid IWorkMonitor.  It may
+        /// return a do-nothing monitor, which should be identical to WorkMonitorSource.EmptyMonitor
+        /// </para>
+        /// </summary>
+        /// <returns>Always a valid IWorkMonitorInstance, although it might not do anything</returns>
+        IWorkMonitor GetMonitor();
+    }
+}
---
+++ Pipeline/WorkMonitorSource.cs
@@ -0,0 +1,52 @@
+﻿using com.antlersoft.HostedTools.Framework.Interface.Plugin;
+using com.antlersoft.HostedTools.Framework.Model;
+using System;
+using System.IO;
+using System.Text;
+
+namespace com.antlersoft.HostedTools.Pipeline
+{
+    public class WorkMonitorSource : HostedObjectBase, IWorkMonitorSource
+    {
+        public static IWorkMonitor EmptyMonitor { get; } = new DoNothingMonitor();
+
+        private IWorkMonitor _currentMonitor = EmptyMonitor;
+
+        public IWorkMonitor SetMonitor(IWorkMonitor newMonitor)
+        {
+            if (newMonitor != null)
+            {
+                _currentMonitor = newMonitor;
+            }
+            return _currentMonitor;
+        }
+
+        private IWorkMonitor ClearMonitor()
+        {
+            _currentMonitor = EmptyMonitor;
+            return _currentMonitor;
+        }
+
+        public IWorkMonitor GetMonitor()
+        {
+            return _currentMonitor;
+        }
+
+        class DoNothingMonitor : HostedObjectBase, IWorkMonitor
+        {
+            DoNothingWriter writer = new DoNothingWriter();
+            public TextWriter Writer => writer;
+
+            public Exception Thrown { get; set; }
+        }
+
+        class DoNothingWriter : TextWriter
+        {
+            public override Encoding Encoding => Encoding.Default;
+
+            public override void Write(char c)
+            {
+            }
+        }
+    }
+}
