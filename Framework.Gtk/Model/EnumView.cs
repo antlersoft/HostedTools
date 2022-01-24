a2880c38aeced05e4b435e804b057500d064d16e
Michael A. MacDonald <mars@antlersoft.com>
2022-01-24 18:18:59 -0500

    Fix EnumView in Gtk

--- Framework.Gtk/Model/EnumView.cs
+++ Framework.Gtk/Model/EnumView.cs
@@ -5,20 +5,25 @@ namespace com.antlersoft.HostedTools.Framework.Gtk.Model
 {
     internal class EnumView : ComboBoxView
     {
+        public EnumView()
+        : base(false)
+        {
+
+        }
+
         private bool _enumsSet;
 
         public override void Reset()
         {
             if (!_enumsSet)
             {
-                _element.Sensitive = false;
                 foreach (object v in Enum.GetValues(Setting.Definition.Type))
                 {
                     _element.AppendText(v.ToString());
                 }
                 _enumsSet = true;
             }
-            _element.Active = Array.IndexOf(Enum.GetValues(Setting.Definition.Type), Setting.GetRaw());
+            _element.Active = Array.IndexOf(Enum.GetValues(Setting.Definition.Type), Enum.Parse(Setting.Definition.Type, Setting.GetRaw()));
             SetNeedsSave(false);
         }
     }
