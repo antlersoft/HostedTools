7ba156e67be82e3c5de3fbdc214e538ecd6992da
mmacdonald86 <michael.macdonald@groundtruth.com>
2024-10-18 18:12:00 -0400

    Update JsonHttpQuery so it can supply additional HTTP headers, and improve error handling

--- Pipeline/JsonHttpQuery.cs
+++ Pipeline/JsonHttpQuery.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.NetworkInformation;
 using com.antlersoft.HostedTools.Framework.Interface.Plugin;
 using com.antlersoft.HostedTools.Framework.Interface.Setting;
 using com.antlersoft.HostedTools.Framework.Interface.UI;
@@ -22,11 +23,12 @@ namespace com.antlersoft.HostedTools.Pipeline
         public static ISettingDefinition QueryUrl = new SimpleSettingDefinition("QueryURL", "JsonHttpQuery", "Query URL");
         public static ISettingDefinition UsePost = new SimpleSettingDefinition("UsePost", "JsonHttpQuery", "Use POST", "If checked, will do POST http query using content from Post Data", typeof(bool), "false");
         public static ISettingDefinition PostData = new MultiLineSettingDefinition("PostData", "JsonHttpQuery", 6, "Post Data");
+        public static ISettingDefinition AdditionalHeaders = new MultiLineSettingDefinition("AdditionalHeaders", "JsonHttpQuery", 4, "Additional Headers");
 
         public JsonHttpQuery()
             : base(
                 new MenuItem("DevTools.Pipeline.Input.JsonHttpQuery", "Json Http Query", typeof (JsonHttpQuery).FullName,
-                    "DevTools.Pipeline.Input"), new[] {QueryUrl.FullKey(), UsePost.FullKey(), PostData.FullKey()})
+                    "DevTools.Pipeline.Input"), new[] {QueryUrl.FullKey(), UsePost.FullKey(), AdditionalHeaders.FullKey(), PostData.FullKey()})
         {
         }
 
@@ -43,6 +45,15 @@ namespace com.antlersoft.HostedTools.Pipeline
                     Content = new StringContent(PostData.Value<string>(SettingManager)),
                     Method = HttpMethod.Post,
                 };
+                String headerText = AdditionalHeaders.Value<string>(SettingManager);
+                foreach (string line in headerText.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    string[] parts = line.Split(new[] {':'}, 2);
+                    if (parts.Length == 2)
+                    {
+                        request.Headers.Add(parts[0], parts[1]);
+                    }
+                }
                 request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
                 message = client.SendAsync(request).Result;
             }
@@ -51,7 +62,10 @@ namespace com.antlersoft.HostedTools.Pipeline
                 message = client.GetAsync(QueryUrl.Value<string>(SettingManager)).Result;
             }
             message.EnsureSuccessStatusCode();
-            yield return JsonConvert.DeserializeObject<IHtValue>(message.Content.ReadAsStringAsync().Result, settings);
+            var convert = JsonConvert.DeserializeObject<IHtValue>(message.Content.ReadAsStringAsync().Result, settings);
+            if (convert != null) {
+                yield return convert;
+            }
         }
 
         public string SourceDescription
@@ -65,7 +79,7 @@ namespace com.antlersoft.HostedTools.Pipeline
 
         public IEnumerable<ISettingDefinition> Definitions
         {
-            get { return new [] {QueryUrl, PostData, UsePost}; }
+            get { return new [] {QueryUrl, PostData, AdditionalHeaders, UsePost}; }
         }
     }
 
