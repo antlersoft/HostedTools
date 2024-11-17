41494a3027d6ff978c12e0b5b1b0f204a0f9d361
mmacdonald86 <michael.macdonald@groundtruth.com>
2024-11-17 00:10:54 -0500

    PersistStem needs to implement IHtValueStem

--- Pipeline/PersistStem.cs
+++ Pipeline/PersistStem.cs
@@ -2,10 +2,16 @@ using System.ComponentModel.Composition;
 
 namespace com.antlersoft.HostedTools.Pipeline {
     [Export(typeof(IStemNode))]
-    public class PersistStem : AbstractNodePersistence<IStemNode>, IStemNode
+    public class PersistStem : AbstractNodePersistence<IStemNode>, IStemNode, IHtValueStem
     {
         protected override string NodeName => "Stem";
 
         protected override string MenuPrefixKey => "Transform";
+
+        public IHtValueTransform GetHtValueTransform(PluginState state)
+        {
+            return PluginManager[state.PluginName].Cast<IHtValueStem>().GetHtValueTransform(state);
+        }
+
     }
 }
