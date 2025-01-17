b57f9b5f34397be3ec6498565e582340d9b0de04
Michael A. MacDonald <mars@antlersoft.com>
2025-01-17 17:17:01 -0500

    Add some tests for ConditionBuilder which was acting funny

---
+++ Tests/ConditionBuilderTest.cs
@@ -0,0 +1,38 @@
+
+using com.antlersoft.HostedTools.ConditionBuilder.Interface;
+using com.antlersoft.HostedTools.ConditionBuilder.Model;
+
+namespace Tests;
+
+public class ConditionBuilderTests {
+    IConditionBuilder builder;
+    [SetUp]
+    public void Setup() {
+        builder = new ConditionBuilder();
+    }
+    
+    [Test]
+    public void BoolLiteralTrue() {
+        var exp = builder.ParseCondition("True");
+    }
+    [Test]
+    public void And() {
+        var exp = builder.ParseCondition("True && True");
+    }
+    [Test]
+    public void MatchAnd() {
+        var exp = builder.ParseCondition("x ~= '((x1)|(x2))' && true");
+    }
+    [Test]
+    public void EqualsAnd() {
+        var exp = builder.ParseCondition("x == '((x1)|(x2))' && true");
+    }
+    [Test]
+    public void AndMatch() {
+        var exp = builder.ParseCondition("true && x ~= '((x1)|(x2))'");
+    }
+    [Test]
+    public void MatchOr() {
+        var exp = builder.ParseCondition("x ~= '((x1)|(x2))' || true");
+    }
+}
